429242316826f26814576a8e8b190b3b39416075
Poornatejas7 <poorna1728@outlook.com>
2024-09-28 21:19:09 +0530

    Add files via upload
    The project converts text to speech

---
+++ text_to_speech.py
@@ -0,0 +1,18 @@
+# ----- Text to Speech ------- #
+
+
+from gtts import gTTS
+from PyPDF2 import PdfReader
+import os
+
+f  =open('startup idea 1.pdf','rb')
+pdf = PdfReader(f)
+text = ''
+for pages in pdf.pages:
+  text += pages.extract_text()
+
+language = 'en'
+
+audio = gTTS(text=text, lang=language,slow=False)
+audio.save('audio.wav')
+os.system('audio.wav')
